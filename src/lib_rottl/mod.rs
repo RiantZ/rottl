c591f26c189f7280e17ec0d196007527f589adcd
Andrey Zheltovskiy <andrey.zheltovskiy@datadoghq.com>
2026-02-02 17:13:06 +0100

    Tests refactoring, split code & tests

--- src/lib_rottl/mod.rs
+++ src/lib_rottl/mod.rs
@@ -1,19 +1,11 @@
 pub mod lexer;
 
-pub use lexer::{Token, Lexer};
+#[cfg(test)]
+mod tests;
+
+pub use lexer::{Lexer, Token};
 
 /// Simple function that takes a string and returns a string
 pub fn get(input: &str) -> String {
     format!("Received: {}", input)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get() {
-        let result = get("hello");
-        assert_eq!(result, "Received: hello");
-    }
 }
